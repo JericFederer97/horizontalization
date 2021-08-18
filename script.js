467e64c8e5c2d783a5442f3c228da707068ee0c6
jeric bandong <jose.enrico_bandong@co-graph.co.jp>
2021-08-18 11:49:41 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector('textarea').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      getVal()
+      document.querySelector('textarea').value = "";
+    }
+});
+
+function getVal() {
+    var val = document.querySelector('textarea').value;
+    copyToClipboard(yokomoji(val));
+    document.querySelector('textarea').value = "";
+    alert("クリップボードにコピー済み\nこのままエクセルシートに貼り付け");
+}
+
+function yokomoji(e) {
+    cleanedData = e.replace("\n", "\t");
+    return cleanedData;
+}
+
+function copyToClipboard(text) {
+    const elem = document.createElement('textarea');
+    elem.value = text;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+ }
